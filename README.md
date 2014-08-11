383607951a84fa517f07abb695cd521d07204938
tommyfok <154274174@qq.com>
2014-08-11 14:37:30 +0800

    feat: 增加预加载功能

--- README.md
+++ README.md
@@ -15,9 +15,11 @@ Simple infinite scrolling for AngularJS
 ###Binding
 ```html
 <div tommy-infinite-scroll="MainCtrl.loadMore()"
+     tommy-infinite-scroll-distance="1"
      tommy-infinite-scroll-disabled="MainCtrl.isLoadMoreDisabled">
 </div>
 ```
 ###Note
 - When scrolling __to the bottom of this element__, execute `tommy-infinite-scroll`'s attribute.
+- `tommy-infinite-scroll-distance` is used for preload the content before user scrolls to the bottom of the element. The unit of this attribute is 'screenHeight'. For example, if it's set to `1`, the container will preload `2 * screenHeight`'s content.
 - Prevent executing `tommy-infinite-scroll`'s attribute by adding `tommy-infinite-scroll-disabled`
--- tommy-angular-infinite-scroll.js
+++ tommy-angular-infinite-scroll.js
@@ -1,15 +1,19 @@
 angular.module('tommy-angular-infinite-scroll', [])
 
-.directive('tommyInfiniteScroll', function ($timeout) {
+.directive('tommyInfiniteScroll', function ($interval) {
   return {
     restrict: 'AE',
     scope: {
       // use `&` to store value as a function body
       reachAction: '&tommyInfiniteScroll',
-      noMoreAction: '=tommyInfiniteScrollDisabled'
+      noMoreAction: '=tommyInfiniteScrollDisabled',
+      distance: '=tommyInfiniteScrollDistance'
     },
     link: function ($scope, $element, $attrs) {
-      var getWindowSize = function () {
+      var distance = angular.isNumber($scope.distance)
+                     ? $scope.distance
+                     : 0;
+      function getWindowSize () {
         return {
           width: window.innerWidth
                  || document.documentElement.clientWidth
@@ -18,26 +22,28 @@ angular.module('tommy-angular-infinite-scroll', [])
                   || document.documentElement.clientHeight
                   || document.body.clientHeight
         };
-      };
+      }
 
-      var getElemRect = function () {
+      function getElemRect () {
         return $element[0].getBoundingClientRect();
-      };
+      }
 
-      var checkBound = function () {
+      function checkBound () {
         if (!!$scope.noMoreAction) {
           return;
         }
 
-        if (getElemRect().bottom - document.body.scrollTop < getWindowSize().height){
+        if (getElemRect().bottom - document.body.scrollTop < getWindowSize().height * (1 + distance)){
           $scope.reachAction();
         }
-      };
+      }
 
-      // use `$timeout` for auto $apply
-      $timeout(checkBound, 0);
-      angular.element(window).on('resize', checkBound);
-      angular.element(window).on('scroll', checkBound);
+      // use `$interval` for auto $apply
+      var timer = $interval(checkBound, 200);
+
+      $scope.$on('$destroy', function () {
+        $interval.cancel(timer);
+      });
     }
   };
 });
